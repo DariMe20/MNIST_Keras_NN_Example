5d2f2a9c51e8dc0459646051d50889560b7b9e01
Daria <med6clj@bosch.com>
2024-09-12 10:59:22 +0300

    Added CNN network

---
+++ keras_model.py
@@ -0,0 +1,29 @@
+from keras import Sequential
+from keras.src.layers import Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense
+
+# Each layer is added one after the other => Sequential arhitecture
+model = Sequential()
+
+# model.add(Lambda(standardize,input_shape=(28,28,1)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dense(512, activation="relu"))
+
+model.add(Dense(10, activation="softmax"))
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.summary()
